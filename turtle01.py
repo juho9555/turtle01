310201717d28e9427705df8919c4c947f52739a2
JUHO <ijuho9555@gmail.com>
2025-07-22 10:22:43 +0900

    장애물 만들기
    장애물 코드 추가

--- turtle01.py
+++ turtle01.py
@@ -7,14 +7,25 @@ s = turtle.getscreen()
 t = turtle.Turtle()
 
 t.shape("turtle")
+
+#장애물 만들기
+for i in range(4):
+    t.fd(100)
+    t.rt(90)
+
+
+#도착점 만들기
 t.penup()
 t.goto(300,100)
 t.pendown()
 t.circle(60)
 t.penup()
+
+#시작점 만들기
 t.goto(-300,-300)
 t.pendown()
 t.circle(60)
 t.penup
 t.setheading(40)
 t.fd(750)
+
