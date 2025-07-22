02fc230f95d8f45e2ba0916f7ff0b0801b1737f8
JUHO <ijuho9555@gmail.com>
2025-07-22 10:16:17 +0900

    수정1
    시작점과 도착점 만들고 출발/도착

--- turtle01.py
+++ turtle01.py
@@ -6,3 +6,15 @@ s = turtle.getscreen()
 #Turtle 변수 지정
 t = turtle.Turtle()
 
+t.shape("turtle")
+t.penup()
+t.goto(300,100)
+t.pendown()
+t.circle(60)
+t.penup()
+t.goto(-300,-300)
+t.pendown()
+t.circle(60)
+t.penup
+t.setheading(40)
+t.fd(750)
